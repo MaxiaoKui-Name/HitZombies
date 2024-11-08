09a492f06ef6b51a4c70dc7c5f15652fdbd965c3
HY-LSZNWIN10\Administrator <634629960@qq.com>
2024-11-08 17:04:19 +0800

    保存最新

--- Assets/Scripts/Local/Controller/PlayerController.cs
+++ Assets/Scripts/Local/Controller/PlayerController.cs
@@ -57,7 +57,7 @@ public class PlayerController : MonoBehaviour
         healthSlider.value = currentValue;
 
         // 初始化移动速度
-        moveSpeed = 2f;//ConfigManager.Instance.Tables.TableGlobal.Get(6).IntValue;
+        moveSpeed = ConfigManager.Instance.Tables.TableGlobal.Get(6).IntValue;
         buffEndScale *= 1.5f;
         // 获取 BuffText 组件并设置为隐藏和缩放为零
         BuffText = transform.Find("PlaySliderCav/BuffText").GetComponent<Text>();
